852da227ba7af0a401f7957aabb9d4622cb19459
Andriy Pan <lexxlexxpan@gmail.com>
2025-05-02 23:39:16 +0300

    4_HomeWork Singleton

---
+++ 4_HomeWork/ProjectPattern/HeaderLogger.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <memory>
+
+//Заданий Інтерфейс LogSink
+struct LogSink {
+
+	virtual void write(const std::string& msg) = 0;
+	virtual ~LogSink() = default;
+
+	LogSink() = default;
+};
+
+//ConsoleSink -> виведення msg на консоль (std::cout)
+class ConsoleSink :public LogSink {
+
+public:
+	void write(const std::string& msg) override {
+		std::cout << msg << "adress sink_  ";
+	}
+};
+
+//FileSink → додає рядок msg до файлу "app.log";
+class FileSink :public LogSink {
+	std::ofstream fout;
+
+public:
+
+	void write(const std::string& msg) override {
+		if (fout.is_open()) {
+			fout << msg << std::endl;
+			std::cout << "\""<<msg<<" This \"msg\" written only to file \"app.log\" adress sink_ ";
+		}
+		else std::cout << "File \"app.log\" cannot be opened\n";
+	}
+
+	FileSink() {
+		fout.open("app.log", std::ios::app);
+	};
+
+	~FileSink() {
+		if (fout.is_open())
+			fout.close();
+	}
+};
+
+//NullSink → нічого не робить;
+class NullSink : public LogSink {
+
+	void write(const std::string& msg) override {
+		(void)msg;
+		std::cout << "WARNING: Ignored input text: adress sink_ ";
+	}
+};
+
+
+enum class SinkType {
+	CONSOLE,
+	FILE,
+	NONE
+};
+
+
+class Logger {
+public:
+	static Logger& instance() {
+		static Logger instance;
+		return instance;
+	}
+	
+	void set_sink(SinkType type) {
+		switch (type) {
+		case SinkType::CONSOLE: sink_ = std::make_unique<ConsoleSink>();  break;
+		case SinkType::FILE: sink_ = std::make_unique<FileSink>(); break;
+		case SinkType::NONE: sink_ = std::make_unique <NullSink>(); break;
+
+		default:
+			throw std::invalid_argument("Error type argument");
+		}
+	}
+
+
+	
+	void log(const std::string& msg) {
+		if (sink_) sink_->write(msg);
+	}
+
+	void adress_sink_() const {
+		std::cout << sink_ << std::endl;
+	}
+	
+
+private:
+
+	std::unique_ptr <LogSink> sink_;
+	
+	Logger() {
+		std::unique_ptr<LogSink> sink_;
+	}
+
+	Logger(const Logger&) = delete;
+	Logger& operator = (const Logger&) = delete;
+
+};
+
---
+++ 4_HomeWork/ProjectPattern/main.cpp
@@ -0,0 +1,57 @@
+﻿#include "HeaderLogger.h"
+#include<iostream>
+
+int main(int argc, char* argv[]) {
+
+	std::string argvStr {};
+	std::string msg {};
+
+	SinkType sinkType_ = SinkType::CONSOLE;
+
+	if (argc !=2) {
+		msg = "No parameter was entered. Default text output to console: ";
+		argvStr = "console";
+	}
+	else {
+		argvStr = argv[1];
+		//Цикл переведення string argument y нижній регістр (при помилковому ввденні)
+		for (int i(0); i < argvStr.size(); i++) {
+			argvStr[i] = std::tolower(argvStr[i]);
+		}	
+	}
+	
+
+	if (argvStr == "console") {
+		msg = "Outputting text to the console: ";
+	}
+	else if (argvStr == "file") {
+		msg = "Entering text into file app.log ";
+		sinkType_ = SinkType::FILE;
+	}
+	else if (argvStr == "none") {
+		msg = "Attention! No action is being performed: ";
+		sinkType_ = SinkType::NONE;
+	}
+	else {
+		std::cout << "Warning: Invalid argument" << std::endl;
+		return 0;
+	}
+	
+	Logger::instance().set_sink(sinkType_);
+
+	std::string tempMsg{};
+	tempMsg = "Test msg_1: " + msg + ":";
+	Logger::instance().log(tempMsg);
+	Logger::instance().adress_sink_();
+
+	tempMsg = "Test msg_2: " + msg + ":";
+	Logger::instance().log(tempMsg);
+	Logger::instance().adress_sink_();
+
+	tempMsg = "Test msg_3: " + msg + ":";
+	Logger::instance().log(tempMsg);
+	Logger::instance().adress_sink_();	
+	
+	system("pause");
+	
+}
